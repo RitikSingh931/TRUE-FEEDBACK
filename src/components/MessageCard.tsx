940a50f0bd9538e8a587ab4085807dfdd9dd0494
RitikSingh931 <ritikroushan931@gmail.com>
2024-06-27 00:07:55 +0530

    type error in key

--- src/components/MessageCard.tsx
+++ src/components/MessageCard.tsx
@@ -23,7 +23,7 @@ import { ApiResponse } from '@/types/ApiResponse';
 
 type MessageCardProps = {
   message: Message;
-  onMessageDelete: (messageId:any) => void;
+  onMessageDelete: (messageId:string) => void;
 };
 
 export function MessageCard({ message, onMessageDelete }: MessageCardProps) {
@@ -37,7 +37,7 @@ export function MessageCard({ message, onMessageDelete }: MessageCardProps) {
       toast({
         title: response.data.message,
       });
-      onMessageDelete(message._id);
+      onMessageDelete(message.id);
 
     } catch (error) {
       const axiosError = error as AxiosError<ApiResponse>;
